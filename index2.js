66f4ea3d310d49fa7bbd826dae433f6cc242b3a2
Rene <racosta323@gmail.com>
2024-01-24 12:36:51 -0600

    updates dropdown and makes it clickable; fixes listener on button

--- index2.js
+++ index2.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    
-    
-    
     loadAPI()
-    // listenForLike()
-
-  
 })
 
 function loadAPI(){
@@ -14,15 +8,7 @@ function loadAPI(){
     .then(data => {
         data.forEach(e=> {
             renderData(e)
-            
-    
         })
-        console.log(data)
-        // let numLikes = document.getElementsByClassName("right")[0].querySelectorAll("h3")[2]
-        // console.log(numLikes)
-        
-       
-        
     });
 }
 
@@ -31,11 +17,10 @@ function renderData(rick){
     const gridHalf = document.getElementById("half")
     const middleImg = document.getElementById("rick-image")
     let numLikes = document.getElementsByClassName("right")[0].querySelectorAll("h3")[2]
-    numLikes.innerText = 1
     const likeButton = document.querySelector("button")
+    const dropdown = document.querySelector("select")
+    const form = document.querySelector("form")
     
-    
-
     //left side
     let nameEl = document.getElementsByClassName("left")[0].querySelector("h3")
     let statusEl = document.getElementsByClassName("left")[0].getElementsByTagName("h3")[1]
@@ -48,8 +33,9 @@ function renderData(rick){
     //creates elements
     const createDivs = document.createElement("div")
     const createImg = document.createElement("img")
+    const createOption = document.createElement("option")
 
-    createImg.addEventListener("click",()=>{
+    createImg.addEventListener("click",(e)=>{
         nameEl.innerText = rick.name
         statusEl.innerText = rick.status
         originEl.innerText = rick.origin
@@ -57,66 +43,52 @@ function renderData(rick){
         locationEl.innerText = rick.location
         middleImg.src = rick.image
         numLikes.innerText = `${rick.likes}`
-
-    
-    likeButton.addEventListener("click", ()=> {increaseLikes(rick, numLikes)})
+        dropdown.selectedIndex = 0
+        createOption.value = rick.name 
     })
 
+    likeButton.addEventListener("click", ()=> {increaseLikes(rick, numLikes)})
 
     // updates elements
     createDivs.className = "card"
     createImg.src = `${rick.image}`
     createImg.alt = `Image of ${rick.name}`
+    createOption.innerText = rick.name
 
     //adds to DOM
     createDivs.append(createImg)
     gridHalf.append(createDivs)
-
-   
+    dropdown.append(createOption)
 
     
+    dropdown.addEventListener("change",(event)=>{
+
+        if(event.target.value === rick.name){
+            nameEl.innerText = event.target.value
+            statusEl.innerText = rick.status
+            originEl.innerText = rick.origin
+            speciesEl.innerText = rick.species
+            locationEl.innerText = rick.location
+            middleImg.src = rick.image
+            numLikes.innerText = `${rick.likes}`
+        }
+    })
 }
 
 
  //DB.JSON SECTION
 
-//  function updateLikes(btn, p, toy){
-//     //select the button and add event listener
-//     // const btn = document.querySelectorAll("button")[1]
-//     // console.log(btn)
-    
-//     btn.addEventListener('click', () =>{
-//       console.log("I've been clicked")
-//       //increment likes
-//       console.log(p.innerText = ++toy.likes)
-  
-      
-  
-//       //get likes element
-  
-  
-  
-//       // toy.likes ++
-//     })
-//  }
-
-
 function increaseLikes(ricks, likes){
     console.log(ricks, likes)
-
-    console.log(likes.textContent) 
-    likes.textContent = ++ricks.likes
-    console.log(likes)
-
-    // let numLikes = document.getElementsByClassName("right")[0].querySelectorAll("h3")[2]
-    // console.log(numLikes)
-    // const likeBtn = document.querySelector("button")
-    // console.log(likeBtn) 
+    const rickH3 = document.getElementsByClassName("left")[0].querySelector("h3").innerText
 
 
-    // likeBtn.addEventListener("click", ()=>{
+    if(rickH3 === ricks.name){
+        console.log(rickH3, ricks.name)
 
-    //     console.log(numLikes)
+        console.log(likes.textContent) 
+        likes.textContent = ++ricks.likes
+        console.log(likes)
 
         fetch(`http://localhost:3000/results/${ricks.id}`,{
             method: "PATCH",
@@ -132,7 +104,7 @@ function increaseLikes(ricks, likes){
                
                 console.log(data.likes)
             })  
-    //     })
+    }
 }
 
 // How I added from API
